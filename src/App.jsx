33973b5dea0ed7c06893f80f649596c67a53a98e
George Olufemi <georgeoluolufemi@gmail.com>
2023-03-13 10:28:43 +0100

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { Container, InsideBox } from './Styles';
+
+function App() {
+  return (
+    <Container>
+      <InsideBox></InsideBox>
+    </Container>
+  )
+}
+
+export default App
+
---
+++ src/Styles.js
@@ -0,0 +1,13 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  height: 200px;
+  width: 200px;
+  background: red;
+`
+
+export const InsideBox = styled.div`
+  height: 100px;
+  width: 100px;
+  background: blue;
+`
